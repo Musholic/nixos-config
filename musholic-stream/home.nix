6dba83b3ee4804c87bf9f203d373330b36479849
Musholic <musholic.tbg@gmail.com>
2025-03-26 13:11:46 +0100

    Enable dark mode by default

--- musholic-stream/home.nix
+++ musholic-stream/home.nix
@@ -103,5 +103,18 @@
         update2 = "nixos-rebuild --use-remote-sudo --show-trace -I nixos-config=/nix/conf switch";
       };
     };
+    dconf.settings = {
+      "org/gnome/desktop/interface" = {
+        color-scheme = "prefer-dark";
+      };
+    };
+
+    gtk = {
+      enable = true;
+      theme = {
+        name = "Adwaita-dark";
+        package = pkgs.gnome.gnome-themes-extra;
+      };
+    };
   };
 }
